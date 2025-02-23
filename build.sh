cf116535ec35b3f66a0f7cf1ff1611f1c69e4742
Jared King <jared.king@subsplash.com>
2025-02-22 16:10:51 -0800

    feat: initial customizations

--- build.sh
+++ build.sh
@@ -2,23 +2,29 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 install -y \
+  1password \
+  code \
+  fira-code-fonts \
+  gcc \
+  gnome-shell-extension-appindicator \
+  gnome-shell-extension-caffeine \
+  gnome-shell-extension-dash-to-dock \
+  make \
+  openvpn3 \
+  openvpn3-indicator \
+  podman \
+  podman-compose \
+  podman-docker \
+  powertop \
+  python3-pip \
+  rsync \
+  tailscale \
+  vim-enhanced \
+  wireguard-tools \
+  zsh
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf5 copr enable -y dsommers/openvpn3
+dnf5 copr enable -y grzegorz-gutowski/openvpn3-indicator
 
 systemctl enable podman.socket
