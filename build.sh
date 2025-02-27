4a09d7773188d0262e309c0dc133953059dbb9bb
Jared King <jared.king@subsplash.com>
2025-02-26 21:04:31 -0800

    fix: openvpn issues and retry 1password

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ set -ouex pipefail
 dnf5 copr enable -y dsommers/openvpn3
 dnf5 copr enable -y grzegorz-gutowski/openvpn3-indicator
 
+mkdir -p /var/opt
+mkdir -p /usr/lib/1Password
+ln -fs /usr/lib/1Password /var/opt/1Password
+
 dnf5 install -y \
+  1password \
   code \
   fira-code-fonts \
   gcc \
@@ -25,3 +30,14 @@ dnf5 install -y \
   zsh
 
 systemctl enable podman.socket
+
+grep -rl "/opt/1Password" /usr/lib/1Password | xargs sed -i 's/\/opt\/1Password/\/usr\/lib\/1Password/g'
+grep -rl "/opt/1Password" /usr/share/applications | xargs sed -i 's/\/opt\/1Password/\/usr\/lib\/1Password/g'
+rm /usr/bin/1password
+ln -s /usr/lib/1Password/1password /usr/bin/1password
+
+mkdir -p /var/lib/openvpn3/configs
+chown -R openvpn:openvpn /var/lib/openvpn3/configs
+chmod 700 /var/lib/openvpn3/configs
+setsebool -P dbus_access_tuntap_device=1
+openvpn3-admin init-config --write-configs
